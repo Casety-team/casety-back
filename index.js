ebdce4186c2a473121a9c20590d9c4aaf2cba4ea
MDrapala <drapalamathieu95@gmail.com>
2020-12-03 07:47:54 +0100

    feat(develop): Create new branch develop

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Initialize project\n');
+  res.end('Create new branch develop\n');
 });
 
 server.listen(port, hostname, () => {
