c3f5f87e700d1a0cd906226ae01d4cc7d8f0c218
MDrapala <drapalamathieu95@gmail.com>
2021-04-12 19:01:21 +0200

    fix: prod

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 module.exports = {
-  HOST: process.env.DB_HOST || "localhost",
-  PORT: process.env.DB_PORT || "8889",
-  USER: process.env.DB_USER || "root",
-  PASSWORD: process.env.DB_PASSWORD || "root",
-  DB: process.env.DB_NAME || "casety",
+  DB_HOST: process.env.DB_HOST || "localhost",
+  DB_PORT: process.env.DB_PORT || "8889",
+  DB_USER: process.env.DB_USER || "root",
+  DB_PASSWORD: process.env.DB_PASSWORD || "root",
+  DB_NAME: process.env.DB_NAME || "casety",
   dialect: "mysql",
   pool: {
     max: 5,
--- app/models/index.js
+++ app/models/index.js
@@ -1,18 +1,23 @@
 const dbConfig = require("../config/db.config");
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  port: dbConfig.PORT,
-  dialect: dbConfig.dialect,
-  operatorsAliases: 0,
-  pool: {
-    max: dbConfig.pool.max,
-    min: dbConfig.pool.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle,
-  },
-});
+const sequelize = new Sequelize(
+  dbConfig.DB_NAME,
+  dbConfig.DB_USER,
+  dbConfig.DB_PASSWORD,
+  {
+    host: dbConfig.DB_HOST,
+    port: dbConfig.DB_PORT,
+    dialect: dbConfig.dialect,
+    operatorsAliases: 0,
+    pool: {
+      max: dbConfig.pool.max,
+      min: dbConfig.pool.min,
+      acquire: dbConfig.pool.acquire,
+      idle: dbConfig.pool.idle,
+    },
+  }
+);
 
 const db = {};
 
