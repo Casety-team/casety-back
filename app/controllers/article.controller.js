54c3d28741f998a5812fd84e9ce5a23de2af1b26
MDrapala <drapalamathieu95@gmail.com>
2021-04-26 18:52:44 +0200

    fix: update article

--- app/controllers/article.controller.js
+++ app/controllers/article.controller.js
@@ -4,7 +4,7 @@ const Op = db.Sequelize.Op;
 
 exports.create = (req, res) => {
   //!empty value
-  if (!req.body.name) {
+  if (!req.body.title) {
     res.status(400).send({
       message: "Content can not be empty!",
     });
