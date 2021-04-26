7479eb1da234abf15e71087051f3cf2ec8c710db
MDrapala <drapalamathieu95@gmail.com>
2021-04-26 10:35:23 +0200

    fix: test veryfpay

--- app/controllers/shop.controller.js
+++ app/controllers/shop.controller.js
@@ -91,25 +91,22 @@ const insertbasket = (
 // ROUTE /api/stripe/success/{token}
 // - Récupère le basket qui a le même token et on valide le paiement
 exports.verifPay = async (req, res) => {
-  let token = req.params.token;
-  res.send("TOKEN: ", token);
-
-  // Basket.findAll({ where: { marketToken: { [Op.eq]: token } } })
-  //   .then((data) => {
-  //     res.send("Success: ", data);
-  //     // data.map(async (r) => {
-  //     // const test = await stripe.paymentIntents
-  //     //   .retrieve(r.paymentIntent)
-  //     //   .then((stripeData) => {
-  //     //     res.send([r, stripeData]);
-  //     //   });
-  //     // });
-  //   })
-  //   .catch((error) => {
-  //     res.status(500).send({
-  //       message: error.message || "Some error occured while retrieving baskets",
-  //     });
-  //   });
+  Basket.findAll({ where: { marketToken: { [Op.eq]: req.params.token } } })
+    .then((data) => {
+      res.send("Success: ", data);
+      // data.map(async (r) => {
+      // const test = await stripe.paymentIntents
+      //   .retrieve(r.paymentIntent)
+      //   .then((stripeData) => {
+      //     res.send([r, stripeData]);
+      //   });
+      // });
+    })
+    .catch((error) => {
+      res.status(500).send({
+        message: error.message || "Some error occured while retrieving baskets",
+      });
+    });
 
   // const getTokenByURL = req.params.code;
   // const toekn = res;
